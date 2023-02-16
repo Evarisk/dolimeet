b1d302f9b6f8a3cab2b0492a4d8f144509c59203
Nicolas Domenech <nicolas@eoxia.com>
2023-02-16 21:16:44 +0100

    #190 [Hook] add: missing currentcontext for sessioncard for saturneBannerTab

--- class/actions_dolimeet.class.php
+++ class/actions_dolimeet.class.php
@@ -226,9 +226,9 @@ class ActionsDolimeet
     public function saturneBannerTab(array $parameters, CommonObject $object): int
     {
         global $db, $langs;
-        
+
         // Do something only for the current context
-        if ($parameters['currentcontext'] == 'saturnecard') {
+        if (in_array($parameters['currentcontext'], ['saturnecard', 'sessioncard'])) {
             if (isModEnabled('contrat')) {
                 require_once DOL_DOCUMENT_ROOT . '/contrat/class/contrat.class.php';
                 if (GETPOST('object_type') == 'trainingsession') {
